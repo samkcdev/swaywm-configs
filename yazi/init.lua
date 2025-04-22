ae3a713c5e7bf1c6d9714dca4133a9355ee9e49c
samkcdev <samnp87@gmail.com>
2025-04-22 12:31:43 +0530

    mimeaps.list-yazi and zellij configs

---
+++ yazi/init.lua
@@ -0,0 +1,14 @@
+require("full-border"):setup{
+	type = ui.Border.ROUNDED,
+}
+
+-- show symlink
+
+Status:children_add(function(self)
+	local h = self._current.hovered
+	if h and h.link_to then
+		return " -> " .. tostring(h.link_to)
+	else
+		return ""
+	end
+end, 3300, Status.LEFT)
